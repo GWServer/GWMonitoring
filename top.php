96cc9f6e387ed6d5865420f30017a0f27bf8fed0
GWServer <33991099+GWServer@users.noreply.github.com>
2024-07-18 14:40:36 +0300

    Add files via upload

---
+++ top.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * V 2.0
+ */
+define( 'MONITORING_URL', 'https://tmonitoring.com/api/check/' );
+define( 'CASE_INSENSITIVE', true );
+
+$config = array(
+
+	'db_host' => 'localhost', //Хост бд
+
+	'db_user' => 'h97352wv_monitor', //Пользователь бд
+
+	'db_pass' => 'Ser1198518', //Пароль от пользователя бд
+	
+	'db_name' => 'h97352wv_monitor', //Название бд
+
+	'db_table' => 'iconomy', //Таблицы iconomy
+
+	'col_money' => 'balance', //Название колонки в таблицы баланса
+
+	'col_user' => 'username', //Название колонки в таблицы логина пользователя
+
+	'amount' => 100, //Сумма начисление за голосование
+
+);
+
+$data = @unserialize( @file_get_contents( MONITORING_URL . (string) $_GET['hash'] . '?id=' . (int) $_GET['id'] ) );
+
+if ( @strlen( $data['hash'] ) != 32 or @strlen( $_GET['hash'] ) != 32 or $data['hash'] != $_GET['hash'] ) {
+	die( "Invalid hash" );
+}
+
+if ( ! $db = @mysqli_connect( $config['db_host'], $config['db_user'], $config['db_pass'], $config['db_name'] ) ) {
+	die( "Could not connect to database" );
+}
+
+$q = "UPDATE `{$config['db_table']}` SET `{$config['col_money']}`=`{$config['col_money']}`+{$config['amount']} WHERE `{$config['col_user']}`" . ( CASE_INSENSITIVE ? " COLLATE utf8_general_ci " : "" ) . "='{$data['username']}'";
+if ( ! @mysqli_query( $db, $q ) ) {
+	die( "An error has occurred" );
+}
